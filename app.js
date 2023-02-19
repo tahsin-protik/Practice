c6a26597f5850b7cc020357a4d65acb316c6ec5d
ARS-shajib <abdurrahmanshajib253@gmail.com>
2023-02-19 18:25:33 +0600

    write file practice

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-const data=require('./sample_data/performance.json');
+const dd=require('./sample_data/performance.json');
+const sorting=require('./services/sorting.js');
+    
+    const data= sorting.sortRes(dd.data);
+
+    const fs= require('fs');
+    const path = require('path')
+    let file_path = "/Practice/";
+    let file_name = "result.json";
+
+    fs.writeFileSync( path.join(__dirname, file_name) , JSON.stringify(data));
+    //console.log(output);
 
